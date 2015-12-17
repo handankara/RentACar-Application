66afdb0773ad34c4afe139851d17cc5f98985cd8
Handan KARA <handan.kara@bil.omu.edu.tr>
2015-12-18 01:59:19 +0200

    Vehicle add and list transactions.

---
+++ RentACarProject/RentACarProject/Controllers/AracListeleController.cs
@@ -0,0 +1,23 @@
+﻿using RentACarProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentACarProject.Controllers
+{
+    public class AracListeleController : Controller
+    {
+        //
+        // GET: /AracListele/
+
+        public ActionResult Index()
+        {
+            var entities = new RentACarDBEntities();
+            var liste = entities.Araclar.ToList();
+            return View(liste);
+        }
+
+    }
+}
---
+++ RentACarProject/RentACarProject/Controllers/AraclarController.cs
@@ -0,0 +1,43 @@
+﻿using RentACarProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentACarProject.Controllers
+{
+    public class AraclarController : Controller
+    {
+        //
+        // GET: /Araclar/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+
+            RentACarDBEntities db = new RentACarDBEntities();
+            Araclar arac = new Araclar();
+
+            arac.aracMarka = form["marka"].Trim();
+            arac.aracModel = form["model"].Trim();
+            arac.aracYakitTipi = form["yakitTipi"].Trim();
+            arac.aracRenk = form["renk"].Trim();
+            arac.aracVitesTipi = form["vitesTipi"].Trim();
+
+            db.Araclar.Add(arac);
+            db.SaveChanges();
+
+            Response.Redirect("/AracListele/Index.cshtml");
+
+            return View();
+           
+         }
+
+    }
+}
