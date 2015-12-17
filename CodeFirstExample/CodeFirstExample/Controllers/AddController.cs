5f0f8cde2e8b0af16fce8698e40b31dba97d5111
Handan KARA <handan.kara@bil.omu.edu.tr>
2015-12-17 16:40:11 +0200

    Membership transactions continues.

---
+++ CodeFirstExample/CodeFirstExample/Controllers/AddController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeFirstExample.Controllers
+{
+    public class AddController : Controller
+    {
+        //
+        // GET: /Add/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ CodeFirstExample/CodeFirstExample/Models/Uye.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CodeFirstExample.Models
+{
+    public class Uye
+    {
+        public int UyeId { get; set; }
+
+        public string Ad { get; set; }
+
+        public string Soyad { get; set; }
+    }
+}
