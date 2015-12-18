d2c11d6c2662e6e664dd9b8c30e60d01a599d447
Handan KARA <handan.kara@bil.omu.edu.tr>
2015-12-18 20:18:39 +0200

    Login transactions continues.

---
+++ RentACarProject/RentACarProject/Controllers/SubelerController.cs
@@ -0,0 +1,24 @@
+﻿using RentACarProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentACarProject.Controllers
+{
+    public class SubelerController : Controller
+    {
+        //
+        // GET: /Subeler/
+
+        public ActionResult Index()
+        {
+            var entities = new RentACarDBEntities();
+            var liste = entities.Subeler.ToList();
+            return View(liste);
+
+        }
+
+    }
+}
